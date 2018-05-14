654214eae562497b6d2ce9f68b4dc0b08f2ca3db
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-05-14 14:05:09 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const handler = function (req, res) {
+  res.end(`Hi. I am actually working. Time on my end is ${new Date}`)
+}
+const server = http.createServer(handler)
+
+server.listen(3000, () => {
+  console.log(`Application running on port 3000`)
+})
